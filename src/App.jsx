7647e7201b1fe8f8466accc7202fad1a64b59cb0
이유신 <2ugod011029@gmail.com>
2025-08-13 22:36:47 +0900

    복구

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+// src/App.jsx
 import React from "react";
-<<<<<<< HEAD
+import { BrowserRouter, Routes, Route, Navigate, Link } from "react-router-dom";
 import "./App.css";
 
-function App() {
+import PostCreate from "./pages/PostCreate";
+import PostList from "./pages/PostList";
+import PostDetail from "./pages/PostDetail";
+import SignUp from "./pages/SignUp";
+
+// ★ 로그인 화면을 별도 컴포넌트로 분리 (원래 상단의 마크업 이동)
+function Login() {
   return (
     <div className="container">
       <div className="logo">LOGO</div>
+
       <div className="input-group">
         <label>닉네임을 입력해주세요</label>
         <input type="text" placeholder="닉네임" />
@@ -16,19 +24,17 @@ function App() {
         <input type="password" placeholder="비밀번호" />
       </div>
 
+      {/* 필요 시 조건부 렌더링으로 바꾸세요 */}
       <p className="error">닉네임과 비밀번호를 확인해주세요</p>
 
       <button className="login-button">접속하기</button>
 
-      <div className="signup">계정 생성하기 {">"}</div>
+      <div className="signup">
+        <Link to="/signup">계정 생성하기 &gt;</Link>
+      </div>
     </div>
-=======
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-
-import PostCreate from "./pages/PostCreate";
-import PostList from "./pages/PostList";
-import PostDetail from "./pages/PostDetail";
-import SignUp from "./pages/SignUp";
+  );
+}
 
 function App() {
   return (
@@ -36,6 +42,9 @@ function App() {
       <Routes>
         {/* 기본 진입은 커뮤니티 목록으로 */}
         <Route path="/" element={<Navigate to="/post" replace />} />
+
+        {/* 로그인 */}
+        <Route path="/login" element={<Login />} />
 
         {/* 회원가입 */}
         <Route path="/signup" element={<SignUp />} />
@@ -50,10 +59,12 @@ function App() {
         <Route path="/post/:id" element={<PostDetail />} />
 
         {/* 404 */}
-        <Route path="*" element={<div style={{ padding: 16 }}>페이지를 찾을 수 없습니다.</div>} />
+        <Route
+          path="*"
+          element={<div style={{ padding: 16 }}>페이지를 찾을 수 없습니다.</div>}
+        />
       </Routes>
     </BrowserRouter>
->>>>>>> 3fd7212447b3a40c40e22c2a5b8a316a2c1d2700
   );
 }
 
